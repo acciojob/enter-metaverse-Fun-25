cb550ab975136677a074c83e1c754c3e4b071908
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-24 19:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn");
+let para=document.getElementById("para");
+
+function changePara(){
+	para.innerText="Entered Metaverse"
+	
+}
+
+btn.addEventListener("click",changePara)
